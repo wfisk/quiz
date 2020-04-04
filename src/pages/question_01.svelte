1b095716fe99578f4915007dc1ae012e5ea30bb5
William FISK <william.fisk@insead.edu>
2020-04-04 17:46:18 +0200

    commit

--- src/pages/question_01.svelte
+++ src/pages/question_01.svelte
@@ -1,11 +1,32 @@
 <script>
   import {
+    onMount
+  } from 'svelte';
+  import {
     fade
   } from 'svelte/transition';
-  // import {
-  //   Howler,
-  //   Howl
-  // } from 'howler';
+  import {
+    Howler,
+    Howl
+  } from 'howler';
+
+  onMount(function() {
+    let sound = new Howl({
+      src: ['/assets/audio/persuaders.mp3']
+    });
+
+    sound.play();
+
+  })
+
+  function playMusic() {
+    let sound = new Howl({
+      src: ['/assets/audio/persuaders.mp3']
+    });
+
+    sound.play();
+
+  }
 
 </script>
 
@@ -17,6 +38,10 @@
 <template>
   <div in:fade="{{delay: 300, duration: 600}}" out:fade="{{delay: 0, duration: 300}}">
     <h1>Question One</h1>
+
+    <button class="btn btn-primary" on:click={playMusic}>
+      Play Music
+    </button>
 
   </div>
 </template>
