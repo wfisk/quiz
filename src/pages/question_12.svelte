95d30e4fe5b4c4c0d8271b1d6cf00e1e89135ff9
INSEAD_ORG\FISK <william.fisk@insead.edu>
2020-04-07 14:00:05 +0200

    commit

--- src/pages/question_12.svelte
+++ src/pages/question_12.svelte
@@ -1,18 +1,15 @@
 <script>
-  import {
-    fade
-  } from 'svelte/transition';
-
+  import { fade } from "svelte/transition";
 </script>
 
-<style>
-
-
-</style>
+<style></style>
 
 <template>
-  <div in:fade="{{delay: 300, duration: 600}}" out:fade="{{delay: 0, duration: 300}}">
-    <h1>Answer 2</h1>
+  <div
+    in:fade="{{delay: 300, duration: 600}}"
+    out:fade="{{delay: 0, duration: 300}}"
+  >
+    <h1>Answer 2222222</h1>
 
     <h2>BBC 1 - Thursday, 6 April 1972</h2>
 
@@ -21,54 +18,49 @@
       <dd class="col-9">Racing at Aintree</dd>
 
       <dt class="col-3">16:20</dt>
-      <dd class="col-9" style="color:red;">Play School</dd>
+      <dd class="col-9" style="color: red;">Play School</dd>
 
       <dt class="col-3">16:45</dt>
-      <dd class="col-9" style="color:red;">The Clangers</dd>
+      <dd class="col-9" style="color: red;">The Clangers</dd>
 
       <dt class="col-3">16:55</dt>
-      <dd class="col-9" style="color:red;">Blue Peter</dd>
+      <dd class="col-9" style="color: red;">Blue Peter</dd>
 
       <dt class="col-3">17:20</dt>
-      <dd class="col-9" style="color:red;">Boss Cat</dd>
+      <dd class="col-9" style="color: red;">Boss Cat</dd>
 
       <dt class="col-3">17:44</dt>
-      <dd class="col-9" style="color:red;">Hector's House</dd>
+      <dd class="col-9" style="color: red;">Hector's House</dd>
 
       <dt class="col-3">17:50</dt>
-      <dd class="col-9" style="color:red;">News</dd>
+      <dd class="col-9" style="color: red;">News</dd>
 
       <dt class="col-3">18:00</dt>
-      <dd class="col-9" style="color:red;">Nationwide</dd>
+      <dd class="col-9" style="color: red;">Nationwide</dd>
 
       <dt class="col-3">18:50</dt>
-      <dd class="col-9" style="color:red;">Tom and Jerry</dd>
+      <dd class="col-9" style="color: red;">Tom and Jerry</dd>
 
       <dt class="col-3">19:00</dt>
-      <dd class="col-9" style="color:red;">Spy Trap</dd>
+      <dd class="col-9" style="color: red;">Spy Trap</dd>
 
       <dt class="col-3">19:25</dt>
-      <dd class="col-9" style="color:red;">Top of the Pops</dd>
+      <dd class="col-9" style="color: red;">Top of the Pops</dd>
 
       <dt class="col-3">20:00</dt>
-      <dd class="col-9" style="color:red;">It's Murder, but is it art?</dd>
+      <dd class="col-9" style="color: red;">It's Murder, but is it art?</dd>
 
       <dt class="col-3">20:30</dt>
-      <dd class="col-9" style="color:red;">The French Way</dd>
+      <dd class="col-9" style="color: red;">The French Way</dd>
 
       <dt class="col-3">21:00</dt>
-      <dd class="col-9" style="color:red;">Nine O'Clock News</dd>
+      <dd class="col-9" style="color: red;">Nine O'Clock News</dd>
 
       <dt class="col-3">21:20</dt>
       <dd class="col-9">Emma's Time</dd>
 
       <dt class="col-3">22:40</dt>
       <dd class="col-9">24 Hours</dd>
-
-
     </dl>
-
-
-
   </div>
 </template>
